744f979fd6c209d79410b4930136aa4248e6dad1
arorar <emailrohitarora@gmail.com>
2017-04-04 17:08:25 -0500

    Say Cheeseeee

--- Homework4/Cheese.R
+++ Homework4/Cheese.R
@@ -12,19 +12,21 @@ modData <- data %>%
   summarise(ni = n(), meanp=mean(logP), meanq = mean(logQ)) %>%
   as.data.frame()
 
-par(mfrow = c(1,2))
+par(mfrow = c(2,2))
 plot(modData$meanp, pch=19,main='Avg Price by Store Over All Weeks', 
      xlab='Store',ylab='Price')
 plot(modData$meanq, pch=19,main='Avg Volume by Store Over All Weeks', 
      xlab='Store',ylab='Vol')
-mfrow(par = c(1,2))
+plot(modData$meanq, modData$meanp, pch=19, 
+     main='Avg Volume vs Price by Store Over All Weeks', 
+     xlab='Vol',ylab='Price')
+par(mfrow = c(1,1))
 
 modData <- data %>% 
   mutate(logP = log(price), logQ = log(vol)) %>%
   as.data.frame()
 
-hlm <- lmer(logP ~  logQ + disp + disp:logP + (1 + logQ  | store), 
-            data = modData)
+hlm <- lmer(logQ ~  (-1 + ( -1 + logP + disp + disp:logP | store)), data = modData)
 
 resid <- ranef(hlm, condVar = T)
 dotplot(resid, scales=list(cex=c(.5,.5)),layout=c(2,1), main=T, 
--- Homework4/CheeseGibbs.R
+++ Homework4/CheeseGibbs.R
@@ -1,70 +1,62 @@
 rm(list = ls())
 
 library(dplyr)
-library(LaplacesDemon)
+library(mvtnorm)
+library(MCMCpack)
 
-# Sampling Model:		y_{it} ~ N(X_{it} beta_i, sig.sq)
+# No fixed effects. Missing beta
+# Sampling Model:		y_{it} ~ N(Z_{it} gamma_i, sig.sq)
 #	Priors:			
-#			beta_i ~ N(mu, Sigma)
-#			(mu,Sigma) ~ Normal-Inv-Wishart(m,v,C,d)
-#			sig.sq ~ 1/sig.sq (Jeffrey's Prior)
+#			gamma_i ~ N(0, Sigma)
+#			Sigma   ~ Inv-Wishart(d,C)
+#			sig.sq  ~ 1/sig.sq (Jeffrey's Prior)
 
-gibbsCheese <- function(y, X, allStores, m, v, C, d,iter= 500, burn=100, thin=2) {
+gibbsCheese <- function(y, Z, allStores, d, C, iter= 100, burn=20, thin=2) {
   
-  n <- length(y); p <- ncol(X); nstores <- nlevels(allStores)
-  beta <- array(0,dim=c(nstores,p,iter))
-  mu <- matrix(0,iter,p); Sigma <- array(0, dim=c(p, p, iter))
-  sig.sq <- rep(0,iter)
+  n <- length(y); p <- ncol(Z); nstores <- nlevels(allStores); sig.sq <- rep(0,iter)
+  gamma <- array(0,dim=c(nstores,p,iter)); Sigma <- array(0, dim=c(p, p, iter))
   
-  beta[,,1] <- rep(0, nstores*p);  mu[1,] <- rep(mean(y),p); Sigma[,,1] <- diag(p); sig.sq[1] <- 1
+  gamma[,,1] <- rep(0, nstores*p);  Sigma[,,1] <- diag(p); sig.sq[1] <- 1
   
   for (iter in 2:iter) {
     
-    #Update beta
+    #Update gamma
     Sig.inv <- solve(Sigma[,,iter-1])
     
-    beta[,,iter] <- 
+    gamma[,,iter] <- 
       t(sapply(1:nstores, function(ns) {	
         
         bStore <- which(ns==as.integer(allStores))
-        Xi <-  X[bStore,]; yi <- y[bStore]; nt <- length(yi)
-        beta.post.var <- solve(Sigma[,,iter-1] + (nt/sig.sq[iter-1]) * crossprod(Xi))
-        beta.post.mean <- beta.post.var %*% (Sig.inv %*% mu[iter-1,] + (nt/sig.sq[iter-1]) * t(Xi) %*% yi )
-        rmvnorm(1, beta.post.mean, beta.post.var)
+        Zi <-  Z[bStore,]; yi <- y[bStore]; nt <- length(yi)
+        gamma.post.var <- solve(Sigma[,,iter-1] + (sig.sq[iter-1])^-1 * crossprod(Zi))
+        gamma.post.mean <- gamma.post.var %*% (sig.sq[iter-1]^-1 * t(Zi) %*% yi)
+        rmvnorm(1, gamma.post.mean, gamma.post.var)
       }))
     
     #Update sig.sq
-    beta.lookup <- beta[as.integer(allStores),,iter]; SS <- sum((y - X %*% t(beta.lookup))^2)
+    gamma.store <- gamma[as.integer(allStores),,iter]; SS <- sum((y - Z %*% t(gamma.store))^2)
     sig.sq[iter] <- 1/rgamma(1, n/2, SS/2)
     
-    #Update mu and Sigma values for NIW
-    beta.bar <- colMeans(beta[,,iter])
-    S <- t(beta[,,iter] - beta.bar)	%*% (beta[,,iter] - beta.bar)
-    
-    mn = (v*m + nstores*beta.bar)/(v + nstores)
-    vn = v + nstores
-    Cn = C + S + (v*nstores / (v+nstores)) * (beta.bar - m) %*% t(beta.bar - m)
+    #Update Sigma values for IW
+    S <- 0; for (s in 1:nstores) S <- S + gamma[s,,iter] %*% t(gamma[s,,iter])
+    Cn = C + S
     dn = d + nstores
     
-    niv.sample <- rnorminvwishart(1, mn, vn, Cn, dn)
-    mu[iter,] <- niv.sample$mu; Sigma[,,iter] <- niv.sample$Sigma
+    Sigma[,,iter] <- riwish(dn, Cn)
   }
   
   thinseq <- seq(1,iter - 1, by=thin)
-  beta = beta[,,-burn]; Sigma = Sigma[,,-burn]; mu = mu[-burn,]; sig.sq = sig.sq[-burn]
-  beta = beta[,,thinseq]; Sigma = Sigma[,,thinseq]; mu = mu[thinseq,]; sig.sq = sig.sq[thinseq]
+  gamma = gamma[,,-burn]; Sigma = Sigma[,,-burn];sig.sq = sig.sq[-burn]
+  gamma = gamma[,,thinseq]; Sigma = Sigma[,,thinseq]; sig.sq = sig.sq[thinseq]
   
   #posterior medians
-  beta.post.median    <- apply(beta, 2, median)
+  gamma.post.median    <- apply(gamma, 2, median)
   sig.sq.post.median  <- median(sig.sq)
-  mu.post.median      <- apply(mu, 2, median)
-  Sigma.post.median   <- Sigma[,iter]
+  Sigma.post.median   <- apply(Sigma, c(1,2), mean)
   
-  list(beta = beta,Sigma = Sigma, mu = mu, sig.sq = sig.sq,
-              beta.post.median = beta.post.median, 
+  list(gamma.post.median = gamma.post.median, 
               sig.sq.post.median = sig.sq.post.median,
-              mu.post.median = mu.post.median,
-              Sigma.post.mean = apply(Sigma, c(1,2), mean))
+              Sigma.post.mean = Sigma.post.median)
 }
 
 data <- read.csv('cheese.csv')
@@ -73,9 +65,8 @@ data <- data %>%
   as.data.frame() %>%
   dplyr::select(-one_of("vol","price"))
 
-y  <- data$logQ; X <- model.matrix(logQ ~ 1 + logP + disp, data=data)
-p <- 3; m <- rep(0, 1); v = 1; C = diag(p); d = p + 1
-
-mcoutput <- gibbsCheese(y, X, data$store, m, v, C, d)
+y  <- data$logQ; Z <- model.matrix(logQ ~ logP + disp, data=data)
+p <- d <- 3; C = diag(p)
+mcoutput <- gibbsCheese(y, Z, data$store, d, C)
 
 
